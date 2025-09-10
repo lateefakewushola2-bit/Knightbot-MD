086b2fe7fc986884cee2024ae500a3447d336f6a
lateefakewushola2-bit <lateefakewushola2@gmail.com>
2025-09-10 02:54:16 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,9 +3,9 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2349137219477', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
